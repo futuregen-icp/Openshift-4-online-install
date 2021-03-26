84d665db591c8807a334eeab308c48e784e19c26
dslee1371 <37610050+dslee1371@users.noreply.github.com>
2021-03-26 11:51:08 +0900

    Create default list to file.cmd

---
+++ Templates/default list to file.cmd
@@ -0,0 +1,7 @@
+## Templates to yaml files
+
++ Template lists to file
+oc get template -n openshift | awk '{print $1}' > list.txt
+
++ Template yaml file to the all as 86EA
+for i in $(cat ./list.txt); do 'oc get template $i -n openshift'; done
