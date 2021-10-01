67b0cb9ed7f3ceee6792f411c51ec04606160405
root <dslee1371@gmail.com>
2021-10-01 04:44:57 -0400

    update 20211001

--- Helth-check/check-etcd.sh
+++
@@ -1,8 +0,0 @@
-while (true)
-do
-  oc rsh -n openshift-etcd etcd-master1.oss2.fu.igotit.co.kr etcdctl endpoint status --cluster -w table >> `hostname`-etcd-health.txt
-  oc rsh -n openshift-etcd etcd-master1.oss2.fu.igotit.co.kr etcdctl endpoint health --cluster -w table >> `hostname`-etcd-health.txt
-  oc rsh -n openshift-etcd etcd-master1.oss2.fu.igotit.co.kr etcdctl member list >> `hostname`-etcd-health.txt
-  date >> `hostname`-etcd-health.txt
-  sleep 30
-done
